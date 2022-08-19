395d0b1a279bffae65a2c940d0679a6aef4f057a
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-19 10:39:30 -0700

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,32 +1,58 @@
 
 
-
 use std::env::{args, Args}
 
-fn main() {
+fn firstFunction() {
     let mut myVar: Args = args();
 
     let first = myVar.nth(0).unwrap(); // use unwrap to get self
-    let myoperator = args.nth(2).unwrap();
+    let myoperator = args.nth(2).unwrap().chars();.next().unwrap();
     let second = args.nth(3).unwrap()
 
-    let my_number: f32 = first.parse().unwrap();
-    let second_number: f32 = second.parse().unwrap()
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number = second.parse::<f32>().unwrap()
+    let result = operate(operator, my_number, second_number);
 
-    println!("{:?}", first, myoperator, second);
+    println!("{:?}", result);
 }
 
 
-fn nth(&mut self, n: usize) - > Option<String> {
-    // assume n = 0;
-    // inner - ["1","2"]
-    self.inner.next()
-    // Calling next again results in second element
-    self.inner.next()
-}
 
 fn nth(&mut self, n: usize) -> Option<String> {
-    inner = ["Hulk","Captain America","Wanda","Vision","Thor","Iron Man","Spiderman"]
-    self.inner.next() // first output: Hulk
-    self.inner.next() // second output: Captain America
+    doctorChoice = ["Doctor 1","Doctor 2","Doctor 3","Doctor 4"]
+    self.inner.next() 
+    self.inner.next() 
+    self.inner.next()
+    self.inner.next()
+}
+
+    // matching expression technique (Rust)
+    fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+        match operator {
+            '8:00' => first_number + second_number,
+            '8:30' => first number + second_number,
+            '9:00' => first_number + second_number,
+            '9:30' => first_number + second_number,
+            '10:00' => first_number + second_number,
+            '10:30' => first_number + second_number,
+            '11:00' => first_number + second_number,
+            '11:30' => first_number + second_number,
+            '1:00' => first_number + second_number,
+            '1:30' => first_number + second_number,
+            '2:00' => first_number + second_number,
+            '2:30' => first_number + second_number,
+            '3:00' => first_number + second_number,
+            '4:00' => first_number + second_number,
+            '4:30' => first_number + second_number,
+            '0:00' => 0.0
+        }
+        }
+
+
+
+
+    fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+        format("{} {} {} = {}", first_number, operator, second_number})
+        println!("{:?}",output(first_number, operator, second_number, result));
+    }
 }
